24e08f564f666f264ddf33e04ab90446da64d242
Sebastian Kratzert <github@kra-se.de>
2013-04-02 15:40:48 +0200

    x

---
+++ cmd_testqueue.c
@@ -0,0 +1,8 @@
+
+#include "cmd_testqueue.h"
+
+static void cmd_test_queue(BaseSequentialStream *chp, int argc, char *argv[])
+{
+	chprintf(chp, "Starting QueueTest\n");
+}
+
---
+++ cmd_testqueue.h
@@ -0,0 +1,6 @@
+#ifndef __CMD_TESTQUEUE_H__
+#define __CMD_TESTQUEUE_H__
+
+static void cmd_test_queue(BaseSequentialStream *chp, int argc, char *argv[]);
+
+#endif
